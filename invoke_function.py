8d39ae6948333ab884ad9429303058efb6edf033
BriefLeaf16 <100491364+BriefLeaf16@users.noreply.github.com>
2022-02-28 12:59:33 -0600

    Add files via upload

---
+++ invoke_function.py
@@ -0,0 +1,38 @@
+#import imp
+
+from initialize_functions import *
+
+'''
+To use a function, you call it. When you call a function, you must provide values, or arguments, for each of the function’s parameters.
+Functions allow you to write code efficiently. When you need to perform an action more than once, wrap that code in a function and call it when you need it. 
+When you need to change how the action is carried out, you can change the code in the function, and the improvement is applied everywhere.
+'''
+
+print("Invoking zero arg function")
+zero_arg_function()
+
+print("Invoking function with required arguments")
+introduction("Harry", "Houdini")
+
+print("Invoking function with default arguments")
+introduction_with_default_args()
+
+#TODO: In this file, go ahead and invoke the rest of the functions from the initialize_functions.py file
+
+print('Invoking function with mix of default arguements')
+introduction_with_mix_of_default_args('John')
+
+print('Invoking function that returns values')
+print(product_of_two_num(9,8))
+print('Function with arbitrary functions')
+print(add_all_nums(1,2,3))
+print('This is the equivalent of the lambda function')
+print(double(12))
+print('This is the recursive fibonacci function')
+print(fib(9))
+print('This is the function level scoping')
+print(subtract(5,2))
+print('This is the palindrome function')
+print(palindrome_str())
+print('Not sure if I was even suppose to make a function, but here is the boolean Bob function')
+print(are_you_bob())
---
+++ test_classes.py
@@ -0,0 +1,34 @@
+from arthmetic import *
+'''
+Classes can represent real-world objects or abstract ideas. After defining a class, you use it by making an instance, or object,of the class. You can make as many instances as you want from one class.
+As an example, you might use a class to represent a website user. The class would have attributes associated with the user’s username, password, registration date, and more. 
+Methods would define the actions the user could take, such as registering, authenticating, logging in, and logging out. 
+You could then make one instance for each user who registers on the site.
+Many external libraries are written as classes, so learn-ing to work with classes makes it easier to work with many existing projects.
+'''
+
+ar = Arithmetic()
+print(ar.add())
+print(ar.divide())
+print(ar.remainder())
+print(type(ar.print_self()))
+
+#TODO: create several more instance of the Arithmetic class and add different values
+print("Next is ar1")
+ar1 = Arithmetic(8,9)
+print(ar1.multiply())
+print(ar1.remainder())
+print("Next is ar2")
+ar2 = Arithmetic(20,50)
+print(ar2.remainder())
+print(ar2.subtract())
+print('Next is ar3')
+ar3 = Arithmetic(3.14,9.80)
+print(ar3.add())
+print(ar3.divide())
+print('Next is ar4')
+ar4 = Arithmetic(-7, -4)
+print(ar4.multiply())
+print(ar4.divide())
+print(ar4.remainder())
+
